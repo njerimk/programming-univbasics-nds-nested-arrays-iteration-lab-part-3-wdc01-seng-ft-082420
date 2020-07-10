af684734a6894eab8015d83c235896bfd172ee4c
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-10 19:14:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ while row_index < src.length do
   element_index= 0
   while element_index < src[row_index].length do
     if src[row_index][element_index].class == string.class
-      src[row_index][element_index] 
+      puts src[row_index][element_index] + "" 
       string = src[row_index][element_index]
       output_string << string
     end
@@ -20,7 +20,7 @@ while row_index < src.length do
   end
   row_index += 1
 end
-puts output_string
+output_string
 end 
 
 

cefc86c9349d160331881bbcaaede8147a6153a5
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-10 18:55:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,17 +8,17 @@ src = [["The",4,"quick"],
 string = ""
 row_index = 0
 output_string =[]
+string_vars =""
 while row_index < src.length do
   element_index= 0
   while element_index < src[row_index].length do
     if src[row_index][element_index].class == string.class
-      string = src[row_index][element_index]
-      
+      string_vars = src[row_index][element_index]
     end
     element_index += 1
   end
   row_index += 1
-  output_string << string
+  output_string << string_vars
 end
 puts output_string
 end 

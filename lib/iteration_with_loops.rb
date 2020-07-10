3d2a67050047550acc7f5a288081761d5a47d3bf
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-10 19:47:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ while row_index < src.length do
   element_index= 0
   while element_index < src[row_index].length do
     if src[row_index][element_index].class == string.class
-        p string = src[row_index]+[element_index]
+        p string = src[row_index][element_index]
        output_string<< string
     end
     element_index += 1
